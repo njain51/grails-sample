97fabc353dbf28c2177c8a6bf597778e4c346fe4
NITIN JAIN <official.nitinjain@gmail.com>
2020-10-05 21:52:00 -0400

    Added first controller

--- grails-app/controllers/myra/InventoryController.groovy
+++ grails-app/controllers/myra/InventoryController.groovy
@@ -2,12 +2,23 @@ package myra
 
 class InventoryController {
 
+    // default action
     def index() {
 
         render "Here is a list of everything"
 
     }
 
+    def edit(){
+
+        def productName = "Breakfast Blend"
+        def sku = "Indian"
+
+        [product:productName, sku:sku]
+
+    }
+
+    // added one more  action
     def remove() {
         render "You have one less item than before"
     }
---
+++ grails-app/views/inventory/edit.gsp
@@ -0,0 +1,18 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: NITINJAIN
+  Date: 10/5/2020
+  Time: 9:44 PM
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+    <title>Edit Inventory</title>
+</head>
+
+<body>
+<h1> ${product} ${sku} </h1>
+
+</body>
+</html>
